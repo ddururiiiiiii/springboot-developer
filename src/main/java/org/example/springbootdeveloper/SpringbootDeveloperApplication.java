eac15bd7054070ecd7eec386aabf8b5713e72ffa
ddururiiiiiii <o244227@daum.net>
2023-06-12 12:46:54 +0900

    first commit

---
+++ src/main/java/org/example/springbootdeveloper/SpringbootDeveloperApplication.java
@@ -0,0 +1,11 @@
+package org.example.springbootdeveloper;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeveloperApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
