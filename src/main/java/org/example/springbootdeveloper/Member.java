359231cebca486c37dac89bdb4ac701de2a67744
ddururiiiiiii <o244227@daum.net>
2023-06-16 06:52:40 +0900

    [레벨1] 03장 스프링부트3 구조 이해하기 (에러해결;)

--- src/main/java/org/example/springbootdeveloper/Member.java
+++ src/main/java/org/example/springbootdeveloper/Member.java
@@ -1,15 +1,9 @@
 package org.example.springbootdeveloper;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonTypeId;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @NoArgsConstructor
 @AllArgsConstructor
